24ec040793da1f054be7ab1f65804099972c6535
hazarsozer <hsozer00@icloud.com>
2026-05-30 21:30:19 +0300

    chore(core): export stats layer from package entry-point
    Barrel index for stats/ submodule; re-export from core index.ts.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,8 @@
 export type { DamageTriple } from "@genshin/types";
 
 export const CORE_VERSION = "0.0.0";
+
+export { Stats } from "./stats/index.js";
+export { StatTable, StatTableAscensionScale } from "./stats/index.js";
+export { applyPostEffects } from "./stats/index.js";
+export type { PostEffect, StatTableAscensionScaleParams } from "./stats/index.js";
---
+++ packages/core/src/stats/index.ts
@@ -0,0 +1,5 @@
+export { Stats } from "./Stats.js";
+export { StatTable, StatTableAscensionScale } from "./StatTable.js";
+export type { StatTableAscensionScaleParams } from "./StatTable.js";
+export { applyPostEffects } from "./postEffects.js";
+export type { PostEffect } from "./postEffects.js";
